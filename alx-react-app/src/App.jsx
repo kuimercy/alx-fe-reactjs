9077401d3a374562d4b03ad8091072de5859a2a7
kuimercy <wanjirumercy2000@gmail.com>
2024-08-18 20:29:20 +0300

    update files

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
+import UserProfile from "./components/UserProfile";
 
 import WelcomeMessage from "./components/WelcomeMessage";
 
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
     </div>
   );
 }
