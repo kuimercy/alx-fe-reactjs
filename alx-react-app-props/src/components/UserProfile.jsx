b094726a3b25e5f9c665543468cca563fa10ff79
kuimercy <wanjirumercy2000@gmail.com>
2024-08-18 23:04:57 +0300

    update files

---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,13 @@
+
+function UserProfile({ name, email }) {
+    return (
+      <div>
+        <h2>User Profile</h2>
+        <p>Name: {name}</p>
+        <p>Email: {email}</p>
+      </div>
+    );
+  }
+  
+  export default UserProfile;
+  
