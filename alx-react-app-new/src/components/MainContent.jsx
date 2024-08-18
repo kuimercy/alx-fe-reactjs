80229c0af24cdf59352c88089addc86b61c24640
kuimercy <wanjirumercy2000@gmail.com>
2024-08-18 22:13:23 +0300

    update files

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: 'lightblue', color: 'white', textAlign: 'center' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+export default MainContent;
