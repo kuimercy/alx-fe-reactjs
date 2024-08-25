761d0a8e10a070b608d7dc35942bdf1865cea467
kuimercy <wanjirumercy2000@gmail.com>
2024-08-25 15:16:49 +0300

    update repository with new directory

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
+
+function App() {
+  return (
+    <div>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
