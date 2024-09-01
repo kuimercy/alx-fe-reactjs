8260fdde7fde86448fbef4b339a8322c527a2792
kuimercy <wanjirumercy2000@gmail.com>
2024-09-01 19:52:55 +0300

    update files

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,69 +1,50 @@
 
 
 
-import { useFormik } from 'formik';
+
+
+
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 const FormikForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      username: '',
-      email: '',
-      password: '',
-    },
-    validationSchema: Yup.object({
-      username: Yup.string().required('Username is required'),
-      email: Yup.string().email('Invalid email address').required('Email is required'),
-      password: Yup.string().required('Password is required'),
-    }),
-    onSubmit: (values) => {
-      console.log('Form submitted:', values);
-    },
+  const initialValues = {
+    username: '',
+    email: '',
+    password: '',
+  };
+
+  const validationSchema = Yup.object({
+    username: Yup.string().required('Username is required'),
+    email: Yup.string().email('Invalid email address').required('Email is required'),
+    password: Yup.string().required('Password is required'),
   });
 
+  const onSubmit = (values) => {
+    console.log('Form submitted:', values);
+  };
+
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <div>
-        <label>Username:</label>
-        <input
-          type="text"
-          name="username"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.username}
-        />
-        {formik.touched.username && formik.errors.username ? (
-          <div style={{ color: 'red' }}>{formik.errors.username}</div>
-        ) : null}
-      </div>
-      <div>
-        <label>Email:</label>
-        <input
-          type="email"
-          name="email"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.email}
-        />
-        {formik.touched.email && formik.errors.email ? (
-          <div style={{ color: 'red' }}>{formik.errors.email}</div>
-        ) : null}
-      </div>
-      <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          name="password"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.password}
-        />
-        {formik.touched.password && formik.errors.password ? (
-          <div style={{ color: 'red' }}>{formik.errors.password}</div>
-        ) : null}
-      </div>
-      <button type="submit">Register</button>
-    </form>
+    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+      <Form>
+        <div>
+          <label>Username:</label>
+          <Field type="text" name="username" />
+          <ErrorMessage name="username" component="div" style={{ color: 'red' }} />
+        </div>
+        <div>
+          <label>Email:</label>
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" component="div" style={{ color: 'red' }} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" component="div" style={{ color: 'red' }} />
+        </div>
+        <button type="submit">Register</button>
+      </Form>
+    </Formik>
   );
 };
 
