e43cf5a7b99636e24388632d33dff0800062ba12
kuimercy <wanjirumercy2000@gmail.com>
2024-09-01 22:56:55 +0300

    update files

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -15,6 +15,18 @@ const useRecipeStore = create((set) => ({
       recipes
     })),
 
+  updateRecipe: (updatedRecipe) => 
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      )
+    })),
+
+  deleteRecipe: (recipeId) => 
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId)
+    })),
+
   addFavorite: (recipeId) => 
     set((state) => ({
       favorites: [...state.favorites, recipeId]
@@ -35,5 +47,6 @@ const useRecipeStore = create((set) => ({
 }));
 
 export default useRecipeStore;
+
 
 
