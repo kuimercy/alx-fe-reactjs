7412d9ee0d59f9c148f1a1c850d6e3fb61a2b329
kuimercy <wanjirumercy2000@gmail.com>
2024-09-15 22:41:38 +0300

    update files

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -1,12 +1,12 @@
-import { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const RecipeDetail = () => {
-  const { id } = useParams(); 
+  const { id } = useParams(); // Get the recipe ID from the URL
   const [recipe, setRecipe] = useState(null);
 
   useEffect(() => {
-    
+    // Fetch recipe data based on the ID
     fetch('/data.json')
       .then((response) => response.json())
       .then((data) => {
@@ -22,27 +22,30 @@ const RecipeDetail = () => {
 
   return (
     <div className="container mx-auto py-8 px-4">
-      <h1 className="text-4xl font-bold mb-4">{recipe.title}</h1>
-      <img className="w-full h-64 object-cover mb-8" src={recipe.image} alt={recipe.title} />
-      <div className="mb-8">
-        <h2 className="text-2xl font-semibold mb-4">Ingredients</h2>
-        <ul className="list-disc list-inside mb-8">
-          {recipe.ingredients.map((ingredient, index) => (
-            <li key={index} className="text-gray-700">{ingredient}</li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <h2 className="text-2xl font-semibold mb-4">Instructions</h2>
-        <ol className="list-decimal list-inside">
-          {recipe.instructions.map((instruction, index) => (
-            <li key={index} className="text-gray-700">{instruction}</li>
-          ))}
-        </ol>
+      <div className="bg-white shadow-lg rounded-lg p-6 mb-8">
+        <h1 className="text-4xl font-bold mb-4">{recipe.title}</h1>
+        <img className="w-full h-64 object-cover mb-8 shadow-md rounded-md" src={recipe.image} alt={recipe.title} />
+        <div className="mb-8">
+          <h2 className="text-2xl font-semibold mb-4">Ingredients</h2>
+          <ul className="list-disc list-inside mb-8">
+            {recipe.ingredients.map((ingredient, index) => (
+              <li key={index} className="text-gray-700">{ingredient}</li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h2 className="text-2xl font-semibold mb-4">Instructions</h2>
+          <ol className="list-decimal list-inside">
+            {recipe.instructions.map((instruction, index) => (
+              <li key={index} className="text-gray-700">{instruction}</li>
+            ))}
+          </ol>
+        </div>
       </div>
     </div>
   );
 };
 
 export default RecipeDetail;
+
 
